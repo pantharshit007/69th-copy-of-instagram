9c9aa3a3c097497125ca01cdbda8fced52b7f65f
pantharshit007 <aaryanpandit3527@gmail.com>
2025-05-25 11:26:39 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-zinc-800">
+      <div className="max-w-md mx-auto h-screen flex flex-col bg-black text-white overflow-hidden place-items-center">
+        hey mom!
+      </div>
+    </div>
+  );
+}
+
+export default App;
